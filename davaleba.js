24654d9c6362800eeb813dd331c49f8451d26ad5
Gvanca77 <abramishvili.gvanca77@gmail.com>
2025-03-30 16:07:38 -0400

    davaleba1

---
+++ davaleba.js
@@ -0,0 +1,51 @@
+let a=87;b=65
+console.log (a+b)
+
+
+
+
+let student = {
+    FirstName: "gvanca",
+    LastName: "abramishvili",
+    age:32,
+    Email:"abramishvili.gvanca77@gmail.com",
+    marks:10.9    
+}
+console. log(student.FirstName)
+
+
+let m=45,n=23
+
+
+ if (m>n){
+    console.log(m+n)
+}
+else{
+    console.log(n)
+}
+
+
+
+
+let studentName=('gvanca')
+console. log("rise and shine,gvanca")
+
+
+let salery=2000,bonus
+if (salery>=2000) bonus=0
+else if (salery<2000) bonus=salery*0.1
+console. log(bonus)
+
+
+let A=23 , B=A/2 ,C=(A+B)
+console. log (C)
+
+
+let phone= ["iphone","samsung","pixel","huawei","xiaomi"]
+console. log (phone)
+
+
+let phone1= ["iphone","samsung","pixel","huawei","xiaomi"]
+console. log (phones[0])
+console. log (phones1[4])
+ 
