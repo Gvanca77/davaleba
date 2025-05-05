2cd7eaa18fc038fbcacb378150be10988f1d1916
Gvanca77 <tinatinabramishvili1992@gmail.com>
2025-05-05 13:54:42 -0400

    Add files via upload

---
+++ cypress/e2e/commands.js
@@ -0,0 +1,19 @@
+Cypress.Commands.add('registerUser', (user) => {
+    cy.visit('https://testzootopia.loremipsum.ge/ka/register')
+    cy.get(':nth-child(1) > .ismile').type(user.fullName)
+    cy.get(':nth-child(2) > .imail').type(user.email)
+    cy.get('.ipir').type(user.idNumber)
+    cy.get(':nth-child(4) > .itel').type(user.phone)
+    cy.get(':nth-child(5) > .ipass').type(user.password)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').type(user.password)
+    cy.get('input[name="agree"]').check({ force: true })
+    cy.get('.regsub').click()
+  })
+  
+  Cypress.Commands.add('loginUser', (credentials) => {
+    cy.visit('https://testzootopia.loremipsum.ge/ka/register')
+    cy.get('.menu-pop > .rprof').click()
+    cy.get(':nth-child(5) > .imail').type(credentials.email)
+    cy.get('.input-shablon > :nth-child(6) > .ipass').type(credentials.password)
+    cy.get('.avtorization > .input-shablon > .form-button').click()
+  })
